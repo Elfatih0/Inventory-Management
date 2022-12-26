e596570b3aeb12dd1aaaf9a57531f3b08feefbf9
Alfatih Nadir Ibrahim <alfatih@amcde.com>
2022-12-26 16:00:19 +0300

    Add project files.

---
+++ Data/InventoryContext.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System;
+using InventoryManagement.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventoryManagement.Data
+{
+    public class InventoryContext:DbContext
+    {
+
+        public InventoryContext(DbContextOptions options):base(options)
+        {
+
+        }
+        
+        
+
+        public virtual DbSet<Unit> Units { get; set; }
+
+    }
+}
---
+++ Models/Units.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace InventoryManagement.Models
+{
+    public class Unit
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string Description { get; set; }
+    }
+}
